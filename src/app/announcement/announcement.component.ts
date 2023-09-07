9446265082b0d641d8499c4ebb47952b66dc0f4f
mitenkumar <mitup7869@gmail.com>
2023-09-08 01:07:00 +0530

    basic Aggrid daata table done

--- src/app/announcement/announcement.component.ts
+++ src/app/announcement/announcement.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { ColDef } from 'ag-grid-community';
 @Component({
   selector: 'app-announcement',
   templateUrl: './announcement.component.html',
   styleUrls: ['./announcement.component.css']
 })
 export class AnnouncementComponent {
+  columnDefs: ColDef[] = [
+    { field: 'make' },
+    { field: 'model' },
+    { field: 'price' }
+];
+
+rowData = [
+    { make: 'Toyota', model: 'Celica', price: 35000 },
+    { make: 'Ford', model: 'Mondeo', price: 32000 },
+    { make: 'Porsche', model: 'Boxster', price: 72000 }
+];
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,23 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { AnnouncementComponent } from './announcement/announcement.component';
 import { AnnouncementRoutingModule } from './announcement/announcement-routing.module';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { AgGridModule } from 'ag-grid-angular';
+import { AnnouncementComponent } from './announcement/announcement.component';
 @NgModule({
-  declarations: [
-    AppComponent,
-    AnnouncementComponent
-  ],
+  declarations: [AppComponent,AnnouncementComponent],
+  
   imports: [
-    CommonModule,
+  CommonModule,
+    HttpClientModule,
+    AgGridModule,
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    AnnouncementRoutingModule
+    AnnouncementRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
